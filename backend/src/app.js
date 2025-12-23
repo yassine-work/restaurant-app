4d3226ffbe223604d33064b67c6ddd8365c623e1
yassine <trabelsi.yassine.work@gmail.com>
2025-12-23 01:54:22 +0100

    backend updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 import express  from "express";
 import reservationRoutes from "./routes/reservationRoutes.js";
 import {errorHandler} from "./middleware/errorHandler.js"
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+
 
 
 const app=express();
@@ -8,6 +14,21 @@ const app=express();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: "Too many requests, please try again later.",
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+// @ts-ignore
+app.use(limiter);
+
 
 app.use("/reservations",reservationRoutes);
 
